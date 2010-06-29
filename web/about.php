3d19e282eddc3852a4e57bd491ad402286e7779f
masubious <masubious@gmail.com>
2010-06-29 19:03:19 +0200

    removed old files

--- web/about.php
+++
@@ -1,7 +0,0 @@
-<div id=about>
-    Just some odd Tools.<br>
-    More detail will follow.<br>
-    
-    for contact: <b>Gar Karath</b> (ingame)<br>
-    or <a href="mailto:admin@masubious.eve-host.com">Mail me</a>
-</div>
