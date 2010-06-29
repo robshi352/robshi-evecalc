d30091f154cff5beff895bbe650d22bd67b96963
masubious <masubious@gmail.com>
2010-06-29 18:11:01 +0200

    new code design

--- web/index.php
+++ web/index.php
@@ -4,18 +4,29 @@ require_once("classes/dbLinkClass.php");
 require_once("classes/siteFunctionsClass.php");
 
     
-    $functions = array("about" => array("about", "about.php"),
-                       "ice" => array("Ice Calculation", "ice.php"),
-                       "pos" => array("POS Refill Calculation", "pos.php"),
-                       "station" => array("Station Locator", "station.php"),
-                       "agent" => array("Agent Locator", "agent.php"),
-                       "t2comps" => array("T2 Component Calculator", "comp.php"));
+    $functions = array("about"      => array( "description" => "about",
+                                              "file"        => "about.php",
+                                              "visible"     => true),
+                       "ice"        => array( "description" => "Ice Reprocessing",
+                                              "file"        => "ice.php",
+                                              "visible"     => false),
+                       "pos"        => array( "description" => "Pos Refill Calculations",
+                                              "file"        => "pos.php",
+                                              "visible"     => true),
+                       "station"    => array( "description" => "Station Locator",
+                                              "file"        => "station.php",
+                                              "visible"     => true),
+                       "agent"      => array( "description" => "Agent Locator",
+                                              "file"        => "agent.php",
+                                              "visible"     => true),
+                       "t2comps"    => array( "description" => "T2 Comp Calculation",
+                                              "file"        => "comp.php",
+                                              "visible"     => true));
 
     $config = parse_ini_file("eveTools.ini", true);
     
     $db = new dbLink($config["db"]["host"], $config["db"]["user"], $config["db"]["password"], $config["db"]["database"]);
     $siteFunctions = new siteFunctions($functions);
-    $siteFunctions->current();
 ?>
 
 <html>
@@ -34,7 +45,7 @@ require_once("classes/siteFunctionsClass.php");
             echo "<div id=content>";
             if ($siteFunctions->getFunctionFile() != "")
             {
-                include($siteFunctions->getFunctionFile($currentFunction));                
+                include($siteFunctions->getFunctionFile());                
             }
             echo "</div>";
             echo "<div style='clear: both;'></div>";
